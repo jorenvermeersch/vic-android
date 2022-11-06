c6ebc8750ac16c2d8ed1a05490f69af7d7474e1c
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-11-06 11:34:11 +0100

    Added data-binding and navigation to fragments

--- app/src/main/java/com/example/vic/screens/customerdetails/CustomerDetailsFragment.kt
+++ app/src/main/java/com/example/vic/screens/customerdetails/CustomerDetailsFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.vic.R
+import com.example.vic.databinding.FragmentCustomerDetailsBinding
 
 class CustomerDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentCustomerDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_customer_details, container, false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/vic/screens/customerlist/CustomerListFragment.kt
+++ app/src/main/java/com/example/vic/screens/customerlist/CustomerListFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.vic.R
+import com.example.vic.databinding.FragmentCustomerListBinding
 
 class CustomerListFragment : Fragment() {
+
+    private lateinit var binding: FragmentCustomerListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_customer_list, container, false)
+        return binding.root
     }
 }
