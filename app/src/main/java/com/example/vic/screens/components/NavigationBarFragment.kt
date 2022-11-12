ace3c92b6de8f03d1c45880dd43a52e46ae5772f
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-11-12 17:38:51 +0100

    Navigation bar in Fragment

---
+++ app/src/main/java/com/example/vic/screens/components/NavigationBarFragment.kt
@@ -0,0 +1,26 @@
+package com.example.vic.screens.components
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.vic.R
+import com.example.vic.databinding.FragmentNavigationBarBinding
+
+class NavigationBarFragment : Fragment() {
+
+    private lateinit var binding : FragmentNavigationBarBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_navigation_bar, container, false)
+
+        return binding.root
+    }
+}
