43362293f8239d53a986f885e18661cd23766cc6
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-11-12 21:01:46 +0100

    Added overflow menu

--- app/src/main/java/com/example/vic/MainActivity.kt
+++ app/src/main/java/com/example/vic/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.vic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import com.example.vic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var toolbar : Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        toolbar = binding.toolbar as Toolbar
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
