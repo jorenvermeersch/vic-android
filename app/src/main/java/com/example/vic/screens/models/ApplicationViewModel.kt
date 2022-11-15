85a5a9504b38262762adddc2e65f729a9a51efb3
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-11-15 14:49:01 +0100

    Added mutable data to ApplicationViewModel

--- app/src/main/java/com/example/vic/screens/models/ApplicationViewModel.kt
+++ app/src/main/java/com/example/vic/screens/models/ApplicationViewModel.kt
@@ -1,9 +1,47 @@
 package com.example.vic.screens.models
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.vic.database.entities.Customer
+import com.example.vic.database.entities.CustomerIndex
+import com.example.vic.database.entities.VirtualMachine
+import com.example.vic.database.entities.VirtualMachineIndex
 
 class ApplicationViewModel : ViewModel() {
-    // selected customer
-    // selected virtual machine
-    //
+
+    // All customers.
+    private val _customers = MutableLiveData<List<CustomerIndex>>()
+    val customers: LiveData<List<CustomerIndex>> get() = _customers
+
+    // All virtual machines of chosen customer.
+    private val _customerMachines = MutableLiveData<List<VirtualMachineIndex>>()
+    val customerMachines: LiveData<List<VirtualMachineIndex>> get() = _customerMachines
+
+    // Customer selected by user.
+    private val _chosenCustomer = MutableLiveData<Customer>()
+    val chosenCustomer: LiveData<Customer> get() = _chosenCustomer
+
+    // Virtual machine selected by user.
+    private val _chosenVirtualMachine = MutableLiveData<VirtualMachine>()
+    val chosenVirtualMachine: LiveData<VirtualMachine> get() = _chosenVirtualMachine
+
+
+    init {
+        _customers.value = listOf()
+        _customerMachines.value = listOf()
+    }
+
+    fun showCustomer(customerId: Long) {
+        // TODO: Fetch customer with given id from database. Set as chosenCustomer.
+        // TODO: Don't forget to fetch virtual machines of customer.
+    }
+
+    fun showVirtualMachine(machineId: Long) {
+        // TODO: Fetch virtual machine with given id from database. Set as chosenVirtualMachine.
+    }
+
+    private fun populate() {
+        // TODO: Generate some mock data for display testing.
+    }
 }
