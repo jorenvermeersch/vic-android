7774285be8c0a38e96212be14e4e812f04eac2a1
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-12-05 17:09:50 +0100

    Split up BindingUtils in different files

---
+++ app/src/main/java/com/example/vic/screens/customerlist/BindingUtils.kt
@@ -0,0 +1,12 @@
+package com.example.vic.screens.customerlist
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.vic.database.entities.CustomerIndex
+
+@BindingAdapter("name")
+fun TextView.setName(item: CustomerIndex?) {
+    item?.let {
+        text = item.name
+    }
+}
