73106b689ee696738aa86582a5151f4d41d2050b
RobinV-Hogent <robin.vermeir@student.hogent.be>
2022-12-20 14:32:10 +0100

    removed unused imports

--- app/src/main/java/com/example/vic/network/CustomerApiService.kt
+++ app/src/main/java/com/example/vic/network/CustomerApiService.kt
@@ -1,13 +1,11 @@
 package com.example.vic.network
 
-import androidx.lifecycle.MutableLiveData
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
--- app/src/main/java/com/example/vic/network/VirtualMachineApiService.kt
+++ app/src/main/java/com/example/vic/network/VirtualMachineApiService.kt
@@ -1,6 +1,5 @@
 package com.example.vic.network
 
-import androidx.lifecycle.MutableLiveData
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
