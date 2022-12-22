79d02559a42d13ef14fda2aa066ae614fccca43e
jorenvermeersch <joren.vermeersch@student.hogent.be>
2022-12-22 23:45:56 +0100

    Added OPS URL

--- app/src/main/java/com/example/vic/network/NetworkConfig.kt
+++ app/src/main/java/com/example/vic/network/NetworkConfig.kt
@@ -3,5 +3,7 @@ package com.example.vic.network
 class NetworkConfig {
     companion object {
         const val BASE_URL = "https://vic-api-custom.vercel.app/api/"
+        // OPS URL:
+        // https://ec2-3-231-54-29.compute-1.amazonaws.com/
     }
 }
